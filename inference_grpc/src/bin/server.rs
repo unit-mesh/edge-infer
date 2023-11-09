2c7c3ec0ae5c1d5b6399557fc027a3646ce51e02
Phodal Huang <h@phodal.com>
2023-11-09 22:54:51 +0800

    chore: try to size

--- inference_grpc/src/bin/server.rs
+++ inference_grpc/src/bin/server.rs
@@ -1,17 +1,20 @@
+use std::path::Path;
 use tonic::{transport::Server, Request, Response, Status};
 
 use tokenizer::tokenizer_server::{Tokenizer, TokenizerServer};
 use tokenizer::{EncodeRequest, EncodeReply};
+
+use inference_core::embed::Semantic;
 
 pub mod tokenizer {
     tonic::include_proto!("tokenizer");
 }
 
 #[derive(Default)]
-pub struct MyGreeter {}
+pub struct MyTokenizer {}
 
 #[tonic::async_trait]
-impl Tokenizer for MyGreeter {
+impl Tokenizer for MyTokenizer {
     async fn encode(
         &self,
         request: Request<EncodeRequest>,
@@ -29,9 +32,11 @@ impl Tokenizer for MyGreeter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse().unwrap();
-    let greeter = MyGreeter::default();
+    let greeter = MyTokenizer::default();
 
     println!("GreeterServer listening on {}", addr);
+
+    // Semantic::initialize(&Path::new(env!("CARGO_MANIFEST_DIR")).join("model")).await.unwrap();
 
     Server::builder()
         .add_service(TokenizerServer::new(greeter))
