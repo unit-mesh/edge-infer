4fbc0974a3e3db4c72163dcdc59d2b9298cd9c7c
Phodal Huang <h@phodal.com>
2023-11-10 23:20:05 +0800

    chore: update for refs

--- inference_rpi/src/main.rs
+++ inference_rpi/src/main.rs
@@ -1,10 +1,14 @@
 use rust_gpiozero::*;
 
+use inference_core::embed::Semantic;
+
+const model: &[u8] = include_bytes!("../../model/model.onnx");
+const tok: &[u8] = include_bytes!("../../model/tokenizer.json");
+
 fn main() {
+    Semantic::initialize(model.to_vec(), tok.to_vec()).unwrap();
     // Create a new LED attached to Pin 17
     let mut led = LED::new(17);
-
     led.blink(1.0, 1.0);
-
     led.wait();
 }
