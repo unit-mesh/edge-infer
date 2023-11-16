9344bd2cb815425389576fc0a802e5f072670340
Phodal Huang <h@phodal.com>
2023-11-16 11:12:48 +0800

    ci: fix tets

--- edge/inference_esp32/src/main.rs
+++ edge/inference_esp32/src/main.rs
@@ -1,14 +1,16 @@
-use inference_core::embed::Semantic;
+use inference_core::Semantic;
 
-const MODEL: &[u8] = include_bytes!("../../model/model.onnx");
-const TOK: &[u8] = include_bytes!("../../model/tokenizer.json");
+
+const MODEL: &[u8] = include_bytes!("../../../model/model.onnx");
+const TOK: &[u8] = include_bytes!("../../../model/tokenizer.json");
 
 #[tokio::main]
 async fn main() {
-    let semantic = Semantic::initialize(MODEL.to_vec(), TOK.to_vec())
-        .await.unwrap();
+    let semantic =
+        Semantic::initialize(MODEL.to_vec(), TOK.to_vec())
+            .await.unwrap();
 
-    let encoding = semantic.embed("Tonic").unwrap();
+    let encoding = semantic.embed("hello, world!").unwrap();
 
     println!("encoding: {:?}", encoding);
 }
