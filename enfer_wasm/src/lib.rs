0c2ba41e5aaaca8a6e7705c55cc5a0712c176891
Phodal Huang <h@phodal.com>
2023-11-27 14:02:42 +0800

    refactor(wasm): init for bundle

--- enfer_wasm/src/lib.rs
+++ enfer_wasm/src/lib.rs
@@ -1,9 +1,14 @@
 mod utils;
+
+use std::sync::Arc;
 use wasm_bindgen::prelude::*;
+use inference_core::{Semantic, SemanticError};
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+
+    // fn init_semantic(model: Vec<u8>, tokenizer_data: Vec<u8>) -> Result<Arc<Semantic>, SemanticError>;
 }
 
 #[wasm_bindgen]
